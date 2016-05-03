b620c99557b1608eb2429bd37227c16df6f3f47b
Aaron Eliason <aaroneliason@hotmail.com>
2016-05-02 18:11:32 -0600

    Alarm and custom cells 90% set up

--- Hologram Alarm(Officail)/SettingsTableViewController.swift
+++ Hologram Alarm(Officail)/SettingsTableViewController.swift
@@ -9,32 +9,59 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        datePickerChanged()
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        
-        return 0
+    @IBAction func datePickerValue(sender: AnyObject) {
+        datePickerChanged()
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return 1
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        
+//        return 0
+//    }
+//
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//        return 0
+//    }
+    
+    func datePickerChanged() {
+        detailLabel.text = NSDateFormatter.localizedStringFromDate(datePicker.date, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-        
-        return cell
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            toggleDatePicker()
+        }
     }
+    var datePickerHidden = false
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if datePickerHidden && indexPath.section == 0 && indexPath.row == 1 {
+            return 0
+        }
+        else {
+            return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+        }
+    }
+    
+    func toggleDatePicker() {
+        datePickerHidden = !datePickerHidden
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+   
 
     
 }
